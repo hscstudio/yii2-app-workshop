42756ed1c438266e7c4b080df002985dcdf5a7f6
unknown <hscstudio>
2015-11-16 03:54:20 +0700

    add mailer config

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2single',
+    'dsn' => 'mysql:host=localhost;dbname=yii2workshop',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
